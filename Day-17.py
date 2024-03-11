0d081206f4c8aac720adfb6ae4fad63aef5d9e21
nmpcgithub <micahkberg@gmail.com>
2024-03-11 10:55:55 -0700

    day 17 pt 1 complete

--- Day-17.py
+++ Day-17.py
@@ -7,6 +7,7 @@ heat_loss_map = load("17")
 
 
 def print_best_path_so_far(cost_map):
+    # prints out a map of heat at different city blocks
     for y in range(goal_y + 1):
         row = "|"
         for x in range(goal_x+1):
@@ -16,10 +17,15 @@ def print_best_path_so_far(cost_map):
                 row += ("----|")
         print(row)
 
-
+# coords for bottom right spot
 goal_x = len(heat_loss_map[0])-1
 goal_y = len(heat_loss_map)-1
-max_cost = ((goal_x * 9) + (goal_y * 9))*2
+
+# wild choice for a max cost
+max_cost_estimate = ((goal_x * 9) + (goal_y * 9)) * 2
+# instead i'll just put the max cost from getting a wrong answer :^)
+small_max_cost = 1070
+#max_cost_estimate = small_max_cost
 
 
 class Node:
@@ -29,18 +35,26 @@ class Node:
         self.coords = (self.x, self.y)
         self.heat = int(heat_loss_map[self.y][self.x])
         self.velocity = velocity
+        self.heat_to_end = self.get_max_heat_to_end()
 
     def get_closeness_to_end(self):
+        # manhattan distance to goal
         return (goal_x-self.x)+(goal_y-self.y)
 
     def get_max_heat_to_end(self):
+        # manhattan distance times max heat of each tile
         return self.get_closeness_to_end() * 9
 
-    def get_tile_evaluation(self, cost_guide, best_answer):
+    def get_tile_evaluation(self, cost_guide, dfs=True):
         #return self.get_closeness_to_end(), self.heat
         #return max(self.velocity)*-1, cost_guide[self], self.heat, self.get_closeness_to_end()
-        if best_answer == max_cost:
-            return self.get_closeness_to_end(), max(self.velocity)*-1, cost_guide[self]+self.get_closeness_to_end()
+
+        # sorting criteria based on if we have reached the end or not
+        # if end not reached, depth first search, else focus on seemingly """best""" points to search from
+
+        if dfs:
+            return self.get_closeness_to_end(), sum(self.velocity) * -1
+            #return cost_guide[self], self.get_closeness_to_end() #, max(self.velocity)*-1, cost_guide[self]+self.get_closeness_to_end()
         else:
             return cost_guide[self] + self.get_closeness_to_end(), max(self.velocity)*-1
 
@@ -57,11 +71,18 @@ class Node:
         if self.x == goal_x and self.y == goal_y:
             return None
         potential_dirs = []
+        # unit vector representing faced direction of lava car
         u_vector = [self.velocity[0]/abs(sum(self.velocity)), self.velocity[1]/abs(sum(self.velocity))]
+
+        # if not going too fast can go straight
         if sum(self.velocity) < 3:
             potential_dirs.append(u_vector)
+
+        # left and right turns
         potential_dirs.append([u_vector[1], u_vector[0]*-1])
         potential_dirs.append([u_vector[1]*-1, u_vector[0]])
+
+        # for each of those above possibilities, create a new node for those positions and return them
         new_nodes = []
         for direction in potential_dirs:
             new_x = self.x + direction[0]
@@ -81,65 +102,76 @@ def find_path():
     nodes_to_test = [Node(0, 0, [1, 0]), Node(0, 0, [0, 1])]
     end_nodes = []
     heat_from_start = {nodes_to_test[0]: 0, nodes_to_test[1]: 0}
-    heat_to_end = {nodes_to_test[0]: None, nodes_to_test[1]: None}
 
-    for x_velocity in range(1,4):
-        new_end_node = Node(goal_x, goal_y, [x_velocity, 0])
+    for velocity in range(1, 4):
+        heat_at_end = heat_loss_map[goal_y][goal_x]
+        # in x direction
+        new_end_node = Node(goal_x, goal_y, [velocity, 0])
         end_nodes.append(new_end_node)
-        heat_to_end[new_end_node] = heat_loss_map[goal_y][goal_x]
-    for y_velocity in range(1,4):
-        new_end_node = Node(goal_x, goal_y, [0, y_velocity])
+        # in y direction
+        new_end_node = Node(goal_x, goal_y, [0, velocity])
         end_nodes.append(new_end_node)
-        heat_to_end[new_end_node] = heat_loss_map[goal_y][goal_x]
 
     true_cost_guide = {nodes_to_test[0].coords: 0, nodes_to_test[1].coords: 0}
-    best_answer = max_cost
+    best_answer = max_cost_estimate
     iters = 0
     while len(nodes_to_test) > 0:
-        nodes_to_test = sorted(nodes_to_test, key=lambda i: i.get_tile_evaluation(heat_from_start, best_answer))
-        current = nodes_to_test.pop(0)
+        if best_answer == max_cost_estimate:
+            nodes_to_test = sorted(nodes_to_test, key=lambda i: i.get_tile_evaluation(heat_from_start, dfs=best_answer == max_cost_estimate))
+            current = nodes_to_test.pop(0)
+        else:
+            current = nodes_to_test.pop(0)
+
+        # print out stuff for debugging purposes
+        if iters % 1000 == 0:
+            nodes_to_test = sorted(nodes_to_test, key=lambda i: i.get_tile_evaluation(heat_from_start, dfs=False))
         if iters % 10000 == 0:
             print(f"loop# {iters}")
             print(f"nodes to test: {len(nodes_to_test)}")
             print(f"current: {current.coords}")
-        if iters % 50000 == 0:
+        if iters % 100000 == 0:
             print_best_path_so_far(true_cost_guide)
         iters += 1
 
+        # if we are over best answer, skip to next one
         if heat_from_start[current] + current.get_closeness_to_end() > best_answer:
-            print("node trimmed")
-            continue
-        if true_cost_guide[current.coords] + current.get_closeness_to_end() > best_answer:
-            print("node trimmed")
+            #print("node trimmed")
+            #print(current)
+            #print(heat_from_start[current])
             continue
 
+        # list of next possible nodes
         next_nodes = current.get_connected_nodes()
 
         for next_node in next_nodes:
+            # heat we would be at for new node
             heat_at_next_node = heat_from_start[current] + next_node.heat
 
-            if heat_at_next_node + next_node.get_closeness_to_end() > best_answer:
-                continue
             if next_node in end_nodes:
-                print("Done!")
-                print(f"part1 : {heat_at_next_node}")
+                print("Found end point!")
                 if heat_at_next_node < best_answer:
+                    heat_from_start[next_node] = heat_at_next_node
+                    true_cost_guide[next_node] = heat_at_next_node
                     best_answer = heat_at_next_node
+                    print(f"potential part1 : {heat_at_next_node}")
                 continue
+            improvement = False
             if next_node not in heat_from_start.keys():
                 heat_from_start[next_node] = heat_at_next_node
-                heat_to_end[next_node] = None
-                if next_node.coords not in true_cost_guide.keys():
-                    true_cost_guide[next_node.coords] = heat_at_next_node
-                elif true_cost_guide[next_node.coords] > heat_at_next_node:
-                    true_cost_guide[next_node.coords] = heat_at_next_node
-                nodes_to_test.append(next_node)
-                continue
-            if heat_from_start[next_node] > heat_at_next_node:
+                improvement = True
+            elif heat_from_start[next_node] > heat_at_next_node:
                 heat_from_start[next_node] = heat_at_next_node
-            if true_cost_guide[next_node.coords] > heat_at_next_node:
-                true_cost_guide[next_node.coords] = heat_at_next_node
+                improvement = True
 
+            if next_node.coords not in true_cost_guide.keys():
+                true_cost_guide[next_node.coords] = heat_at_next_node
+                improvement = True
+            elif true_cost_guide[next_node.coords] > heat_at_next_node:
+                true_cost_guide[next_node.coords] = heat_at_next_node
+                improvement = True
+
+            if improvement and heat_at_next_node + next_node.get_closeness_to_end() < best_answer:
+                nodes_to_test.append(next_node)
     print(best_answer)
 
 find_path()
