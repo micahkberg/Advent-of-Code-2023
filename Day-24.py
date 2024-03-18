01c798fea4514835fbb179e619a81412655a5511
nmpcgithub <micahkberg@gmail.com>
2024-03-18 14:54:02 -0700

    day24 with some algebra notes as i try to figure out how linear algebra works again

--- Day-24.py
+++ Day-24.py
@@ -35,6 +35,11 @@ class Hailstone:
             return False
         return True
 
+    def __repr__(self):
+        coord = (self.x, self.y, self.z)
+        vel = (self.dx, self.dy, self.z)
+        return f"coord: {coord}, velocity: {vel}"
+
 
 def read_hailstones():
     lines = load("24")
@@ -55,8 +60,59 @@ def main():
         if pair[0].intersect2d(pair[1]):
             intersection_count += 1
     print(intersection_count)
-
-
-
+    print(read_hailstones())
 
 main()
+
+"""
+part 2:
+there are hailstones H
+for each hailstone they have linear parametric equations H_nxyz_n = {x0_n + tdx_n}, {y0_n + tdy_n}, {z0_n + tdz_n}
+we are given each of the coord_0 values and slopes
+we need to find the line that intersects all of these worldlines
+Rock = Hn(t)
+
+i think i can start by just finding a line that intersects 2 lines and check it against a 3rd line
+then keep going til it hits all lines?
+
+line that passes through xn,yn,zn,tn
+
+19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
+
+x1 = 19 - 2t       x2 = 18 - t
+y1 = 13 + t         y2 = 19 - t
+z1 = 30 - 2t        z2 = 22 - 2t
+
+x = x0 + mt
+y = y0 + nt
+z = z0 + lt
+
+x0 + mt_1 = 19 - 2t_1       18 - t_2 = x0 + mt_2
+x0 = 19 - 2t_1 - mt_1       18 - x0  = t_2 + mt_2
+x0 = 10 - t_1 (2+m)         x0  = t_2(1 + m) + 18
+
+10 - t_1 (2+m) = t_2 (1 + m) +18
+-t_1 * (2+m) = t_2(1+m) + 8
+
+
+(x0 - x)/m = t_1
+
+
+
+"""
+
+
+def mainpt2():
+    stones = read_hailstones()
+    a = stones[0]
+    b = stones[1]
+    t = 0
+    while True:
+
+
+
+        t+=1
